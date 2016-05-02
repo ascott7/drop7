0eefef052a1dfc23b03f1682a1980dada629ba31
Andrew Scott <ascott@hmc.edu>
2016-05-01 18:49:28 -0700

    now running each individual 4 times to evaluate fitness

--- neat_nn.py
+++ neat_nn.py
@@ -1,51 +1,57 @@
 from __future__ import print_function
 from neat import nn, population, visualize
 from drop7 import Game
+import numpy as np
 import random, pickle
+from multiprocessing import Pool
 
 
 class Learner:
     def __init__(self):
         #config = Config('drop7_config')
         pop = population.Population('drop7_config')
-        pop.run(self.eval_fitness, 2000)
+        pop.run(self.eval_fitness, 100)
 
         print('Number of evaluations: {0}'.format(pop.total_evaluations))
 
         # Display the most fit genome.
-        winner = pop.statistics.best_genome()
-        print('\nBest genome:\n{!s}'.format(winner))
+        self.winner = pop.statistics.best_genome()
+        print('\nBest genome:\n{!s}'.format(self.winner))
 
         # Verify network output against training data.
-        print('\nOutput:')
-        winner_net = nn.create_feed_forward_phenotype(winner)
+        #print('\nOutput:')
+        self.winner_net = nn.create_feed_forward_phenotype(self.winner)
         #for inputs, expected in zip(xor_inputs, xor_outputs):
         #        output = winner_net.serial_activate(inputs)
         #            print("expected {0:1.5f} got {1:1.5f}".format(expected, output[0]))
 
         visualize.plot_stats(pop.statistics)
         visualize.plot_species(pop.statistics)
-        #visualize.draw_net(winner, view=True, filename="xor2-all.gv")
-        #visualize.draw_net(winner, view=True, filename="xor2-enabled.gv", show_disabled=False)
-        #visualize.draw_net(winner, view=True, filename="xor2-enabled-pruned.gv", show_disabled=False, prune_unused=True)
+        visualize.draw_net(self.winner, view=True, filename="drop7-all.gv")
+        visualize.draw_net(self.winner, view=True, filename="drop7-enabled.gv", show_disabled=False)
+        visualize.draw_net(self.winner, view=True, filename="drop7-enabled-pruned.gv", show_disabled=False, prune_unused=True)
 
-
+    def make_move(self, game):
+        moves = self.winner_net.serial_activate(game_to_sit(game))
+        moves_tuples = zip(moves, range(len(moves)))
+        moves_tuples.sort()
+        for move in moves_tuples:
+            if game.place_piece(move[1]):
+                break
+        
     def eval_fitness(self, genomes):
         for gen in genomes:
             net = nn.create_feed_forward_phenotype(gen)
-            game = Game(print_board=False)
-            while not game.game_over:
-                moves = net.serial_activate(game_to_sit(game))
-                #move = moves.index(max(moves))
-                moves_tuples = zip(moves, range(len(moves)))
-                moves_tuples.sort()
-                for move in moves_tuples:
-                    if game.place_piece(move[1]):
-                        break
+            #scores = np.zeros(5, dtype=np.int)
+            p = Pool(4)
+            scores = p.map(simulate_game, [(net, False)] * 4)
+            p.close()
+            #for i in range(4):
+                
                 #if not game.place_piece(move):
                     #print("Tried to make illegal move :(")
                 #    break
-            gen.fitness = game.points
+            gen.fitness = np.median(np.array(scores))
             
             #sum_square_error = 0.0
             #for inputs, expected in zip(xor_inputs, xor_outputs):
@@ -57,6 +63,19 @@ class Learner:
                 # its maximum value of 1.0.
                 #g.fitness = 1 - sum_square_error
 
+def simulate_game(tup):
+    net, classic_mode = tup
+    game = Game(print_board=False, sleep=False, classic_mode=classic_mode)
+    while not game.game_over:
+        moves = net.serial_activate(game_to_sit(game))
+        #move = moves.index(max(moves))
+        moves_tuples = zip(moves, range(len(moves)))
+        moves_tuples.sort()
+        for move in moves_tuples:
+            if game.place_piece(move[1]):
+                break
+    return game.points
+        
 def game_to_sit(game):
     # 10 * 49 + 2 means 492 input nodes
     """sit = []
@@ -78,7 +97,7 @@ def game_to_sit(game):
 def naive_scores(iterations):
     scores = []
     for i in range(iterations):
-        g = Game(print_board=False)
+        g = Game(print_board=False, classic_mode=False)
         while not g.game_over:
             choices = g.available_cols()
             choice = random.choice(choices)
@@ -89,10 +108,11 @@ def naive_scores(iterations):
 if __name__ == "__main__":
     #print(naive_scores(100))
     l = Learner()
-    output = open('learner.pkl', 'wb')
+    output = open('learner2.pkl', 'wb')
     pickle.dump(l, output)
     output.close()
 
-    pkl_file = open('learner.pkl', 'rb')
-    learner = pickle.load(pkl_file)
-    pkl_file.close()
+    #pkl_file = open('learner2.pkl', 'rb')
+    #learner = pickle.load(pkl_file)
+    #pkl_file.close()
+    #simulate_game((learner.winner_net, False))
